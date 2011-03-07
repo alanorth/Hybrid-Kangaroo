931ad1b13a083847a74d6f743e8282ed48320a31
Alan Orth <alan.orth@gmail.com>
2011-03-07 15:12:57 -0500

    Initial commit based on Hybrid Original 0.2

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Functions File
+ *
+ * This is your child theme's functions.php file.  It is here for adding custom functions and 
+ * setting up default functionality for this child theme.  You are free to modify this file in
+ * any way you like.
+ *
+ * @package HybridOriginal
+ * @subpackage Functions
+ */
+
+?>
