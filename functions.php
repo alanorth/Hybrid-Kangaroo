1516ee70aa1baa08a7fb4979fa72dccfc835baf3
Alan Orth <alan.orth@gmail.com>
2011-10-30 16:07:27 -0400

    Whitespace fix

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /**
  * Functions File
  *
- * This is your child theme's functions.php file.  It is here for adding custom functions and 
+ * This is your child theme's functions.php file.  It is here for adding custom functions and
  * setting up default functionality for this child theme.  You are free to modify this file in
  * any way you like.
  *
